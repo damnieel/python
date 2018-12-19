70e1cd2e1abc4a8e33efb9df666eb80311a8450e
damnieel <331712034@qq.com>
2018-12-19 17:49:42 +0800

    1

--- python/src/helloworld.py
+++ python/src/helloworld.py
@@ -172,7 +172,127 @@ else:
     print('00后')
     
 '''
-
-循环
+第一种是for...in循环
+第二种循环是while循环，只要条件满足，就不断循环，条件不满足时退出循环。比如我们要计算100以内所有奇数之和，可以用while循环实现：
+break
+在循环中，break语句可以提前退出循环。例如，本来要循环打印1～100的数字：
+continue
+在循环过程中，也可以通过continue语句，跳过当前的这次循环，直接开始下一次循环。
+'''
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+    
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum = sum + x
+print(sum)
 
 '''
+Python提供一个range()函数，可以生成一个整数序列，再通过list()函数可以转换为list
+'''
+print list(range(5))
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
+sum = 0
+n = 99
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
+
+'''
+dict
+Python内置了字典：dict的支持，dict全称dictionary，在其他语言中也称为map，使用键-值（key-value）存储，具有极快的查找速度。
+为什么dict查找速度这么快？因为dict的实现原理和查字典是一样的。假设字典包含了1万个汉字，我们要查某一个字，一个办法是把字典从第一页往后翻，直到找到我们想要的字为止，这种方法就是在list中查找元素的方法，list越大，查找越慢。
+
+第二种方法是先在字典的索引表里（比如部首表）查这个字对应的页码，然后直接翻到该页，找到这个字。无论找哪个字，这种查找速度都非常快，不会随着字典大小的增加而变慢。
+
+dict就是第二种实现方式，给定一个名字，比如'Michael'，dict在内部就可以直接计算出Michael对应的存放成绩的“页码”，也就是95这个数字存放的内存地址，直接取出来，所以速度非常快。
+你可以猜到，这种key-value存储方式，在放进去的时候，必须根据key算出value的存放位置，这样，取的时候才能根据key直接拿到value。
+
+把数据放入dict的方法，除了初始化时指定外，还可以通过key放入：
+由于一个key只能对应一个value，所以，多次对一个key放入value，后面的值会把前面的值冲掉：
+
+要避免key不存在的错误，有两种办法，一是通过in判断key是否存在：
+二是通过dict提供的get()方法，如果key不存在，可以返回None，或者自己指定的value：
+返回None的时候Python的交互环境不显示结果。
+要删除一个key，用pop(key)方法，对应的value也会从dict中删除：
+'''
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print(d['Michael'])
+d['Adam'] = 67
+print(d)
+d['Jack'] = 90
+print(d)
+d['Jack'] = 88
+print(d)
+
+print('Thomas' in d)
+print('Jack' in d)
+
+'''
+返回None的时候Python的交互环境不显示结果。
+'''
+print(d.get('Thomas', 0))
+print(d.get('Thomas'))
+print(d.get('Thomas', -1))
+d.pop('Bob')
+print(d)
+
+'''
+请务必注意，dict内部存放的顺序和key放入的顺序是没有关系的。
+
+和list比较，dict有以下几个特点：
+
+查找和插入的速度极快，不会随着key的增加而变慢；
+需要占用大量的内存，内存浪费多。
+而list相反：
+
+查找和插入的时间随着元素的增加而增加；
+占用空间小，浪费内存很少。
+所以，dict是用空间来换取时间的一种方法。
+
+dict可以用在需要高速查找的很多地方，在Python代码中几乎无处不在，正确使用dict非常重要，需要牢记的第一条就是dict的key必须是不可变对象。
+
+这是因为dict根据key来计算value的存储位置，如果每次计算相同的key得出的结果不同，那dict内部就完全混乱了。这个通过key计算位置的算法称为哈希算法（Hash）。
+
+要保证hash的正确性，作为key的对象就不能变。在Python中，字符串、整数等都是不可变的，因此，可以放心地作为key。而list是可变的，就不能作为key：
+'''
+
+'''
+set
+set和dict类似，也是一组key的集合，但不存储value。由于key不能重复，所以，在set中，没有重复的key。
+要创建一个set，需要提供一个list作为输入集合：
+'''
+s = set([1, 2, 3])
+print s
+s = set([1, 1, 2, 2, 3, 3])
+print s
+s.add(4)
+print s
+s.remove(4)
+print s
+
+'''
+上面我们讲了，str是不变对象，而list是可变对象。
+对于可变对象，比如list，对list进行操作，list内部的内容是会变化的，比如：
+
+要始终牢记的是，a是变量，而'abc'才是字符串对象！有些时候，我们经常说，对象a的内容是'abc'，但其实是指，a本身是一个变量，它指向的对象的内容才是'abc'
+当我们调用a.replace('a', 'A')时，实际上调用方法replace是作用在字符串对象'abc'上的，而这个方法虽然名字叫replace，但却没有改变字符串'abc'的内容。相反，replace方法创建了一个新字符串'Abc'并返回，如果我们用变量b指向该新字符串，就容易理解了，变量a仍指向原有的字符串'abc'，但变量b却指向新字符串'Abc'了
+'''
+a = ['c', 'b', 'a']
+a.sort()
+print a
+
+a = 'abc'
+a.replace('a', 'A')
+print a
+
+a = 'abc'
+b = a.replace('a', 'A')
+print b
