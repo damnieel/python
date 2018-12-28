b0876105218ace7bb18769ea2a7c1132a7f17308
damnieel <331712034@qq.com>
2018-12-28 13:46:59 +0800

    1

--- python/src/helloworld.py
+++ python/src/helloworld.py
@@ -306,6 +306,8 @@ print (int('123'))
 print (hex(123))
 
 def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
     if x >= 0:
         return x
     else:
@@ -317,3 +319,18 @@ def my_pass(x):
     pass
 print(my_abs(-99))
 print(my_pass(-99))
+
+import math
+
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+x, y = move(100, 100, 60, math.pi / 6)
+print(x, y)
+'''
+Python函数返回的仍然是单一值： 返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，而多个变量可以同时接收一个tuple，按位置赋给对应的值，所以，Python的函数返回多值其实就是返回一个tuple，但写起来更方便。
+'''
+r = move(100, 100, 60, math.pi / 6)
+print(r)
